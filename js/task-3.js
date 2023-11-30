5701c689be16823f330ea63509b51ac79fa7cd80
Alina <alinazolotopup99@gmail.com>
2023-11-30 18:12:03 +0100

    task-1, task-2

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+function filterArray(numbers, value) {
+  value.indexOf(numbers); }
+// let massive = []
+  // for(let i = 0; i < value; i ++) {
+  //   // if (massive.includes(i)) {
+  //     massive.push(i);
+  //   // }
+  // }
+  //   // return massive
+  // }
+  
+
+  
+
+  console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+
