7bd68adf851705927721b1883b95ae7d4f39371e
Alina <alinazolotopup99@gmail.com>
2023-12-09 21:31:49 +0100

    update-task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,14 @@
 function filterArray(numbers, value) {
-  value.indexOf(numbers); }
-// let massive = []
-  // for(let i = 0; i < value; i ++) {
-  //   // if (massive.includes(i)) {
-  //     massive.push(i);
-  //   // }
-  // }
-  //   // return massive
-  // }
-  
-
-  
-
-  console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-
+  let massive = [];
+  for (let i = 0; i < numbers.length; i ++) {
+    if (numbers[i] > value)
+      massive.push(numbers[i]);
+    }
+    
+    return massive;
+  }
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
